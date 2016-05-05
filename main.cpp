aa4aff4433a49f7b771007ba90c0387706e7cc2f
[dawidles] <[226090@student.pwr.edu.pl]>
2016-05-05 19:51:43 +0200

    Rozwiazanie

--- main.cpp
+++ main.cpp
@@ -7,17 +7,27 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
+	int n=atoi(argv[1]);
+	srand(argc==3?atoi(argv[2]):time(NULL));
 	drawCharSquare(charSquare(n), n);	
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square = new char *[n*n];
+
+	for (int i=0; i<n*n; i++){
+		square[i] = new char[n*n];
+		square[i/n][i%n] = char(rand()%26 + 97); // lub 'a' + rand()%('z'-'a' +1); prosilbym o informacje, ktory zapis bedzie "poprawniejszy"
+	}
+	
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
-
+	for(int i=0; i<n*n; i++){
+		printf("%c ", square[i/n][i%n]);
+			if ((i%n) == (n-1)) printf("\n");
+		}
 }
